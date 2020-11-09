6ac79bb51120287f4ca4d953bdec7bb80ce341ff
intrusive <kanittha.wo@ku.th>
2020-11-09 16:59:32 +0700

    🎨lightning design system :: force:recordData

---
+++ aura/accEdit/accEditController.js
@@ -0,0 +1,33 @@
+({
+    handleSaveRecord: function(component, event, helper) {
+        component.find("recordEditor").saveRecord($A.getCallback(function(saveResult) {
+            if (saveResult.state === "ERROR") {
+                component.set('v.recordSaveError', JSON.stringify(saveResult.error));
+            } else {
+                component.set('v.recordSaveError', "");
+            }
+        }));
+    },
+
+    handleRecordUpdated: function(component, event, helper) {
+        var eventParams = event.getParams();
+        if(eventParams.changeType === "CHANGED") {
+            // get the fields that are changed for this record
+            var changedFields = eventParams.changedFields;
+            console.log('Fields that are changed: ' + JSON.stringify(changedFields));
+            // record is changed so refresh the component (or other component logic)
+            var resultsToast = $A.get("e.force:showToast");
+            resultsToast.setParams({
+                "title": "Saved",
+                "message": "The record was updated."
+            });
+            resultsToast.fire();
+        } else if(eventParams.changeType === "LOADED") {
+            // record is loaded in the cache
+        } else if(eventParams.changeType === "REMOVED") {
+            // record is deleted and removed from the cache
+        } else if(eventParams.changeType === "ERROR") {
+            console.log('Error: ' + component.get("v.error"));
+        }
+    }
+})
