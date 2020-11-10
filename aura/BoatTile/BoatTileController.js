58031f41fbffe7fdda5be9cbe5cd0ea5b8e98963
intrusive <kanittha.wo@ku.th>
2020-11-10 11:21:10 +0700

    🎉 [AuraSpecialist] :: challenge 10

--- aura/BoatTile/BoatTileController.js
+++ aura/BoatTile/BoatTileController.js
@@ -3,7 +3,7 @@
         var data=component.get("v.boat");
         console.log("data received in boattile"+data);
     },
-   
+
     onBoatClick : function(component, event, helper) {
         var data=event.getSource().get("v.value")
         var boatid=data.Id;
@@ -13,12 +13,20 @@
             "boatId":boatid
         });
         BoatSelect.fire();
-       
+
         //fire BoatSelected event
-        var boatselected = $A.get("e.c:BoatSelected");
+        var boatselected = $A.get("e.c:BoatSelected"); 
         boatselected.setParams({
             "boat" : data
-        });
+        }); 
         boatselected.fire();
+        var plotMapMarkerEvent = $A.get("e.c:PlotMapMarker");
+        plotMapMarkerEvent.setParams({
+            "sObjectId" : selectedBoat.Id,
+            "lat" : selectedBoat.Geolocation__Latitude__s,
+            "long" : selectedBoat.Geolocation__Longitude__s,
+            "label" : selectedBoat.Name
+        });
+        plotMapMarkerEvent.fire();
     },
 })
